46aecd93fe5d9032ce8882a25d656ef4cc9cbe7b
cutmybeans <calvinvanlaaren@outlook.com>
2022-03-26 18:50:42 +0100

    Opdracht 2 maar weet niet 100% of ie goed is maar hij geeft geen erros ig? Maar ksnap die hele test ding niet echt dus idk of ie goed is vandaar.

--- BAI - InleverOpdrachten - 2/Opdracht2.cs
+++ BAI - InleverOpdrachten - 2/Opdracht2.cs
@@ -12,24 +12,53 @@ namespace BAI___InleverOpdrachten___2
         {
             HashSet<uint> set = new HashSet<uint>();
             // *** IMPLEMENTATION HERE *** //
+            set = inputStroom.ToHashSet();
             return set;
         }
         public static HashSet<uint> ZonderLicht(List<uint> inputStroom)
         {
             HashSet<uint> set = new HashSet<uint>();
             // *** IMPLEMENTATION HERE *** //
+            foreach (var item in inputStroom)
+            {
+                if (Opdracht1.Licht(item))
+                {
+                    inputStroom.Remove(item);
+                }
+            }
+            set = inputStroom.ToHashSet();
             return set;
         }
         public static HashSet<uint> MetWagon(List<uint> inputStroom)
         {
             HashSet<uint> set = new HashSet<uint>();
             // *** IMPLEMENTATION HERE *** //
+            foreach (var item in inputStroom)
+            {
+                if (!Opdracht1.Wagon(item))
+                {
+                    inputStroom.Remove(item);
+                }
+            }
+            set = inputStroom.ToHashSet();
             return set;
         }
         public static HashSet<uint> SelecteerID(List<uint> inputStroom, uint lower, uint upper)
         {
             HashSet<uint> set = new HashSet<uint>();
             // *** IMPLEMENTATION HERE *** //
+            foreach (var item in inputStroom)
+            {
+                if (Opdracht1.ID(item) > lower && Opdracht1.ID(item) < upper)
+                {
+                    
+                }
+                else
+                {
+                    inputStroom.Remove(item);
+                }
+            }
+            set = inputStroom.ToHashSet();
             return set;
         }
     }
